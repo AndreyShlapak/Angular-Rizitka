60f4d5acb9a289fad14735ba185a6338bb8c5325
AndreyShlapak <andreyshlapak12@gmail.com>
2022-11-07 23:18:21 +0200

    added filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HeaderComponent } from './components/header/header.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { GalleryCardsComponent } from './components/gallery-cards/gallery-cards.component';
 import { GalleryCardComponent } from './components/gallery-card/gallery-card.component';
 import { GalleryFiltersComponent } from './components/gallery-filters/gallery-filters.component';
 import {MatCardModule} from "@angular/material/card";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [
@@ -25,17 +26,19 @@ import {MatCardModule} from "@angular/material/card";
     GalleryCardComponent,
     GalleryFiltersComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatInputModule,
-    FormsModule,
-    MatCardModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatInputModule,
+        FormsModule,
+        MatCardModule,
+        MatCheckboxModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/gallery-filters/gallery-filters.component.ts
+++ src/app/components/gallery-filters/gallery-filters.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {FormArray, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-gallery-filters',
   templateUrl: './gallery-filters.component.html',
-  styleUrls: ['./gallery-filters.component.scss']
+  styleUrls: ['./gallery-filters.component.scss'],
 })
 export class GalleryFiltersComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    brand: new FormArray([]),
+    cpu: new FormArray([]),
+    memory: new FormArray([]),
+  });
 
-  constructor() { }
+  criterionNames: string[] = Object.keys(this.form.controls);
+
+  private readonly CONTROL_AMOUNT = 20;
 
   ngOnInit(): void {
+    this.generateControls();
   }
 
+  private generateControls(): void {
+    for (const controlName of this.criterionNames) {
+      for (let i = 0; i < this.CONTROL_AMOUNT; i++) {
+        const control: FormControl = new FormControl<boolean>(false);
+
+        (this.form.get(controlName) as FormArray).push(control);
+      }
+    }
+  }
 }
